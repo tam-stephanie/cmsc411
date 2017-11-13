410061fa19dba44ddd07ee24937c967f98491bdd
bhazlet <hben1@umbc.edu>
2017-11-13 10:22:25 -0500

    updated cordic.py

--- cordic.py
+++ cordic.py
@@ -2,14 +2,13 @@ import math
 
 ANGLE = 80.1
 
-#dividend/divisor 
-#what about is dividend = 0?
+
 def division(dividend, divisor, precision):
     whole = 0 
     iterate = 0
     while iterate < precision:
         
-        while dividend >= divisor:      
+        while dividend >= divisor:
             whole += 1 * 2**(-iterate)
             dividend -= divisor
             
@@ -19,17 +18,56 @@ def division(dividend, divisor, precision):
         iterate += 1
 
     return whole
+
         
+
+
+def cordic_h(target, angles, tanh):
+    # z
+    currAngle = target
+    # y 
+    currSinh = 0
+    # x
+    currCosh = find_An_hyper(len(angles))
+
+    for i in range(0, len(angles)):
+
+        if currAngle <= 0:
+            currAngle += angles[i]
+            
+            newCosh = currCosh + (currSinh * 2.0**(-i))
+            newSinh = currSinh - (currCosh * 2.0**(-i))
+            
+            currCosh = newCosh
+            currSinh = newSinh
+
+
+        elif currAngle > 0:
+            currAngle -= angles[i]
+
+            newCosh = currCosh - (currSinh * 2.0**(-i))
+            newSinh = currSinh + (currCosh * 2.0**(-i))
+
+            currCosh = newCosh
+            currSinh = newSinh
+
+
+    return currSinh, currCosh
+
+
+
+
+
+
 
 # Performs the cordic rotation to calculate sine and cosine
 def cordic(target, angles, tan, sin, cos):
     # z
     currAngle = target
-
     # y
     currSin = 0
     # x
-    currCos = gain(cos)
+    currCos = 1 / find_An(len(angles))
 
     for i in range(0, len(angles)):
 
@@ -63,6 +101,22 @@ def gain(cosTable):
 
     return gain
 
+
+def find_An(n):
+    An = math.sqrt(2)
+    for i in range(1,n):
+        An = An * math.sqrt(1 + 2**(-2*i))
+    print (An)
+    return An
+
+
+def find_An_hyper(n):
+    An = math.sqrt(2)
+    for i in range(1,n):
+        An = An * math.sqrt(1 - 2**(-2*i))
+    return An
+
+
 def main():
 
     anglesTan = [45.0, 26.6, 14.0, 7.1, 3.6, 1.8, 0.9, 0.4, 0.2, 0.1]
@@ -73,12 +127,15 @@ def main():
 
     cos = [0.707, 0.894, 0.970, 0.992, 0.998, 0.999, 1, 1, 1, 1]
 
+    
     corSin, corCos = cordic(ANGLE, anglesTan, tan, sin, cos)
-#   tangent(ANGLE, anglesTan, tan)
-    
-    
+       
     corTan = division(corSin,corCos,10)
 
+    corSinh, corCosh = cordic_h(ANGLE, anglesTan, tan)
+
+    print(corSinh, corCosh)
+    
     print("angle =", ANGLE)
     print("sin =", corSin, "cos =", corCos, "tan =", corTan)
 
