2b01aed3045f013063565920ceef55f1ad433fcd
Stephanie Tam <ru14618@umbc.edu>
2017-11-28 20:07:52 -0500

    updating sin & cos

--- cordic.s
+++ cordic.s
@@ -1,6 +1,6 @@
 .data
 GAIN_CONST:
-  .float 0.60725
+  .float 1.646768
 
 anglesTan:
   .float  45.0, 26.565, 14.0362, 7.12502, 3.57633
@@ -22,11 +22,15 @@ main:
 
   LDR   R0, =currAngle      @ load address of angle
   LDR   R3, [R0]            @ store angle
-
+  FMSR  S0, R3              @ load into floating-point register
 
   LDR   R0, =GAIN_CONST     @ load address of gain constant
   LDR   R4, [R0]            @ store GAIN_CONST
+  FMSR  S1, R4              @ load into fp reg
 
 for_loop:
   ADD   R1, R1, #1          @ increment loop counter
   MOV   R3, R1
+
+
+  VCMP  S0, 0
